a8166f42947061d1d3e6e55ee743b510e1daf280
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-11-06 04:02:35 -0800

    Learned about forms/inputs in react, as well as controlled components

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -3,18 +3,23 @@ import React from 'react';
 class NameForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {textValue: '', selectValue: 'coconut'};
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {
-      this.setState({value: event.target.value});
+      // destructuring name and value from event.target.
+      const {name, value} = event.target
+      // Using an index signature to determine which state to update based on the event.target's name, sets state to teh event.targets 
+      // value. 
+      this.setState({[name]: value});
     }
   
     handleSubmit(event) {
       alert('A name was submitted: ' + this.state.value);
+      // utilizing the synthetic event input to prevent default behaviour of the form refreshing the page on submit
       event.preventDefault();
     }
     
@@ -29,11 +34,25 @@ class NameForm extends React.Component {
     // An input whose value is controlled by React in this way is called a controlled component
     render() {
       return (
+        // To handle multiple form elements with one eventHandler give each element a name attribute, can then use an index signature 
+        // to determine what state to change based on the name of the event.target. Should make the name the same as the state that controls 
+        // the input.
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input name='textValue' type="text" value={this.state.textValue} onChange={this.handleChange} />
           </label>
+          <label>
+          {/* Dropdown values are chosen by passing the state and event listener on the select tag, initial choice is determined by 
+          initial state. you can  */}
+          Pick your favorite flavor:
+          <select name='selectValue' value={this.state.selectValue} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
           <input type="submit" value="Submit" />
         </form>
       );
